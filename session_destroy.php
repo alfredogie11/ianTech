b9414722e9484be09f4f6ba1ef9905d015f5431c
Ogie11 <benitez.alfredo.b.1128@gmail.com>
2022-02-16 21:13:57 +0800

    unang push

--- session_destroy.php
+++ session_destroy.php
@@ -2,6 +2,7 @@
 session_start();
 $_SESSION['username']="";
 $_SESSION['acc_type']="";
+$_SESSION['action_cat']="";
 if(isset($_SESSION['query'])){
    $_SESSION['query']="SELECT * FROM items ORDER BY category DESC";
 }
